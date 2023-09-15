e3ab5ba2e9ca5e814daf6c250e352d96e4f50b4a
srs-sieam <srssieam@gmail.com>
2023-09-15 15:27:53 +0600

    changed title and fixed cart height

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const Cart = ({selectedCourse, creditHour, remainingHours, coursePrice}) => {   // step-8) here it has received the selected course from App.jsx
     // console.log(selectedCourse, 'hiii')
     return (
-        <div className="md:w-1/3 lg:w-1/4 bg-white shadow-md p-6 rounded-xl">
+        <div className="md:w-1/3 lg:w-1/4 bg-white shadow-md p-6 rounded-xl h-min">
             <h3 className="text-md lg:text-lg font-bold text-blue-600">Credit Hour Remaining {remainingHours} hr</h3>
             <hr className="my-4"/>
             <h3 className="font-bold text-lg lg:text-xl">Course Name</h3>
